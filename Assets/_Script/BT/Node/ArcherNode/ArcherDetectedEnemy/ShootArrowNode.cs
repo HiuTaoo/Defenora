881bcf34ddbefc9724c6709cc372f2b85cbd1e13
HiuTao <kaze2k3@gmail.com>
2026-06-05 18:08:28 +0700

    fix archer anim

--- Assets/_Script/BT/Node/ArcherNode/ArcherDetectedEnemy/ShootArrowNode.cs
+++ Assets/_Script/BT/Node/ArcherNode/ArcherDetectedEnemy/ShootArrowNode.cs
@@ -1,16 +1,15 @@
-﻿using UnityEngine;
+﻿using _Script.Unit_Management_System.Animation;
+using UnityEngine;
 
 namespace _Script.BT.Node.ArcherNode.ArcherDetectedEnemy
 {
     public class ShootArrowNode : BTActionNode
     {
-        private readonly float animDuration = 1f;
         private readonly Archer archer;
         private bool hasShot;
 
         private int lastFrameChecked = -1;
         private BTStatus lastStatus = BTStatus.Running;
-        private float timer;
 
         public ShootArrowNode(Unit unit) : base(unit)
         {
@@ -21,7 +20,7 @@ namespace _Script.BT.Node.ArcherNode.ArcherDetectedEnemy
         {
             var target = archer.archerBlackBoard.detectedEnemy;
 
-            if (target == null)
+            if (target == null && !hasShot)
             {
                 ResetInternal();
                 return BTStatus.Failure;
@@ -31,6 +30,9 @@ namespace _Script.BT.Node.ArcherNode.ArcherDetectedEnemy
             {
                 if (lastStatus == BTStatus.Running)
                 {
+                    ArcherFireDirection fireDir = archer.archerBlackBoard.fireDirection;
+                    if (fireDir != ArcherFireDirection.None) archer.animFSM.SetFireDirection(fireDir);
+                    
                     archer.animState = AnimState.Attacking;
                     archer.currentState = UnitState.Attack;
                 }
@@ -40,42 +42,52 @@ namespace _Script.BT.Node.ArcherNode.ArcherDetectedEnemy
 
             lastFrameChecked = Time.frameCount;
 
-            if (!hasShot)
+            if (hasShot && archer.isAttacking)
             {
-                archer.animFSM.SetFireDirection(archer.archerBlackBoard.fireDirection);
                 archer.currentState = UnitState.Attack;
                 archer.animState = AnimState.Attacking;
-                hasShot = true;
-                timer = 0f;
+                lastStatus = BTStatus.Running;
+                return BTStatus.Running;
             }
 
-            archer.currentState = UnitState.Attack;
-            archer.animState = AnimState.Attacking;
-
-            timer += Time.deltaTime;
-
-            if (timer >= animDuration)
+            if (hasShot && !archer.isAttacking)
             {
                 archer.nextFireTime = Time.time + archer.attackCooldown;
 
+                archer.EndAttackSignal();
                 archer.ResetAnim();
                 archer.currentState = UnitState.Idle;
 
                 ResetInternal();
 
                 lastStatus = BTStatus.Success;
-                return BTStatus.Success;
+                return BTStatus.Success; 
             }
 
-            lastStatus = BTStatus.Running;
+            if (!hasShot)
+            {
+                ArcherFireDirection fireDir = archer.archerBlackBoard.fireDirection;
+                if (fireDir != ArcherFireDirection.None) archer.animFSM.SetFireDirection(fireDir);
+                
+                archer.StartAttackSignal();
+
+                archer.currentState = UnitState.Attack;
+                archer.animState = AnimState.Attacking;
+                
+                hasShot = true;
+                lastStatus = BTStatus.Running;
+                return BTStatus.Running;
+            }
+
             return BTStatus.Running;
         }
 
         public override void ClearState()
         {
             base.ClearState();
-            if (archer != null && archer.animState == AnimState.Attacking)
+            if (archer != null && hasShot)
             {
+                archer.EndAttackSignal();
                 archer.ResetAnim();
                 archer.currentState = UnitState.Idle;
             }
@@ -86,7 +98,6 @@ namespace _Script.BT.Node.ArcherNode.ArcherDetectedEnemy
         private void ResetInternal()
         {
             hasShot = false;
-            timer = 0f;
             lastFrameChecked = -1;
             lastStatus = BTStatus.Running;
         }
